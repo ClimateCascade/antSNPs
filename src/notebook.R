3be45bc0306c447bf6fa10a5c2492072d8221533
MKLau <matthewklau@fas.harvard.edu>
2014-12-13 17:34:57 -0500

    update

--- src/notebook.R
+++ src/notebook.R
@@ -11,4 +11,6 @@ x <- x[(1:length(x))[c(F,T)]]
 library(ape)
 snp = rbind(A=c(1,1,1,0,1),B=c(1,1,0,1,1),D=c(1,1,1,1,1),E=c(0,1,0,1,0))
 stree = nj(dist.gene(snp))
+png('rplot.png')
 plot(stree)
+dev.off()
