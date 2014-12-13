2953f51bb006ad90001765f268f0ce1964d2394e
MKLau <matthewklau@fas.harvard.edu>
2014-12-13 18:39:31 -0500

    update

--- src/notebook.R
+++ src/notebook.R
@@ -11,7 +11,10 @@ x <- read.fasta('/N/dc2/scratch/scahan/sandbox/UVM_ant_sequences.fas')
 set.seed(12345)
 x.clust <- phyclust.edist(x$org)
 x.nnt <- nj(x.clust)
+x.rnt <- root(x.nnt,outgroup=1:5,resolve.root=TRUE)
+x.rnt <- multi2di(x.nnt)
+x.draw <- x.rnt
 png('rplot.png')
-plot(x.nnt)
+plot(x.draw)
 dev.off()
 
