7718cb341d0357eb289c69e7e7b01ee177e4b115
mkl <matthewklau@fas.harvard.edu>
2015-02-23 14:30:30 -0500

    update

--- src/waAnalyses.R
+++ src/waAnalyses.R
@@ -1,7 +1,11 @@
 ###warm ant population genetic analyses
 
 ## sample information 
+library('seqinr')
 library('gdata')
-info <- read.xls('/N/u/mklau/Mason/Dropbox/WarmAntDimensions/Genomics/RADseq_mastersheet_2014.xlsx')
+info <- read.xls('../data/RADseq_mastersheet_2014.xlsx')
+
+
 
 ## sequence information
+ddrad1 <- read.fasta('')
