7407c9e96ec2f725b1d82ac3416c1767809e9170
mkl <matthewklau@fas.harvard.edu>
2015-02-24 09:41:41 -0500

    incorporating all sequences into genetic distance analysis

--- src/waAnalyses.R
+++ src/waAnalyses.R
@@ -1,50 +1,18 @@
 ###warm ant population genetic analyses
 
-## sample information 
-library('gdata')
-library('seqinr')
-library('ape')
-source('./waFunctions.R')
-info <- read.xls('../data/RADseq_mastersheet_2014.xlsx')
-
-## sequence information
-rad1 <- read.dna('../data/UVM_ant_sequences.fasta',format='fasta')
-
-## associate rad labels with master sheet labels
-site.labs <- toupper(paste(as.character(info$Site),info$Collection_no,sep=''))
-site.labs <- sub('BRM /BRF','BRF',site.labs);site.labs <- sub('IJAMSQL','IJAMS2',site.labs);site.labs <- sub(" \\(SECOND SAMPLE\\)","42",site.labs)
-phyt.labs <- as.character(info$Phytotron_ID..Ap_GXL.)
-phyt.labs <- sub('-','',phyt.labs)
-seq
-rad.labs <- toupper(attributes(rad1)$dimnames[[1]])
-rad.labs <- sub('00','',sub('_TRIMMED_FILTERED','',rad.labs))
-rad.labs <- sub('IJANSQ','IJAMS',rad.labs);rad.labs <- sub('IJANS','IJAMS',rad.labs);rad.labs <- sub('DSDF','DSF',rad.labs)
-rad.labs <- sub('-','',rad.labs)
-rad.labs[rad.labs%in%phyt.labs] <- site.labs[match(rad.labs,phyt.labs,nomatch=0)]
-
-###Remove specimens that do not have species ID's
-seqs <- read.fasta('../data/UVM_ant_sequences.fasta')
-if (all(attributes(rad1)$dimnames[[1]]==names(seqs))){
-  info.rad <- info[match(rad.labs,site.labs),]
-  rad1.ids <- seqs[as.character(info.rad$Species) != '']
-  print(length(rad1.ids));rad1.ids <- waQC(rad1.ids);print(length(rad1.ids))
-  write.fasta(rad1.ids, names=names(rad1.ids), file.out='../data/UVM_ant_ids.fasta')
-}else{warning('Sorted names do not match!')}
-###
-
 ###Only analyze identified specimens
-rad1 <- read.dna('../data/UVM_ant_ids.fasta',format='fasta')
+### rad1 <- read.dna('../data/UVM_ant_ids.fasta',format='fasta') ###only samples with species ids
+### rad1 <- read.dna('../data/UVM_ant_ids.fasta',format='fasta') ###only samples with species ids
+rad1 <- read.dna('../data/UVM_ant_sequences.fasta',format='fasta') ###all sequences
+info <- info <- read.xls('../data/RADseq_mastersheet_2014.xlsx')
 
-###sid.info <- info[site.labs%in%rad.labs & as.character(info$Species) != '',]
-rad.labs <- toupper(attributes(rad1)$dimnames[[1]])
-rad.labs <- sub('00','',sub('_TRIMMED_FILTERED','',rad.labs))
-rad.labs <- sub('IJANSQ','IJAMS',rad.labs);rad.labs <- sub('IJANS','IJAMS',rad.labs);rad.labs <- sub('DSDF','DSF',rad.labs)
-rad.labs <- sub('-','',rad.labs)
+site.labs <- toupper(paste(as.character(info$Site),info$Collection_no,sep=''));site.labs <- sub('BRM /BRF','BRF',site.labs);site.labs <- sub('IJAMSQL','IJAMS2',site.labs);site.labs <- sub(" \\(SECOND SAMPLE\\)","42",site.labs)
+rad.labs <- waSampleIDs(x=attributes(rad1)$dimnames[[1]])
 rad.labs[rad.labs%in%phyt.labs] <- site.labs[match(rad.labs,phyt.labs,nomatch=0)]
 info.rad <- info[match(rad.labs,site.labs),]
 rad.labs <- paste(rad.labs,info.rad[,4],info.rad[,5])
 
-rad1.d <- dist.dna(rad1,model='raw')
+rad1.d <- dist.dna(rad1,model='')
 rad1.m <- as.matrix(rad1.d)
 attr(rad1.d,'Labels') <- rad1.labs <- rad.labs
 rownames(rad1.m) <- colnames(rad1.m) <- rad1.labs
--- src/waFunctions.R
+++ src/waFunctions.R
@@ -1,5 +1,12 @@
 ###Warm Ant Dimensions SNP data processing functions
 
+waSampleIDs <- function(x='sequence labels'){
+  rad.labs <- toupper(x)
+  rad.labs <- sub('00','',sub('_TRIMMED_FILTERED','',rad.labs))
+  rad.labs <- sub('IJANSQ','IJAMS',rad.labs);rad.labs <- sub('IJANS','IJAMS',rad.labs);rad.labs <- sub('DSDF','DSF',rad.labs)
+  rad.labs <- sub('-','',rad.labs)
+  return(rad.labs)
+}
 
 waQC <- function(seqs,quality.threshold = 0.8){
   ac <- apply(do.call(rbind,lapply(seqs,table)),1,function(x) x/sum(x))
